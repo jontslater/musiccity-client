809ce7f1a71c059fb50a1c996e2b0f207f43a68d
jonathan slater <jonslater2005@gmail.com>
2024-02-03 12:35:40 -0600

    working on view post card

---
+++ .history/pages/viewPost_20240203114124.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { useAuth } from '../utils/context/authContext';
+
+export default function viewPost() {
+  const [postDetails, setPostDetails] = useState({});
+  const router = useRouter();
+
+  useEffect(() => {
+    viewPostDetails(id).then(setPostDetails);
+  }, [id]);
+import React from 'react';
+
+export default function Profile() {
+  return (
+    <>
+      <div className="card mb-3">
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <img src="https://m.media-amazon.com/images/I/61yQAF86FKL._AC_SY450_.jpg" className="rounded float-start" alt="..." />
+          <h1>Post title here</h1>
+          <h5>author here</h5>
+          <h7 className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></h7>
+        </div>
+      </div>
+
+      <div className="card">
+        <button type="button">like</button>
+        <button type="button">love</button>
+        <button type="button">celebrate</button>
+        <button type="button">dislike</button>
+      </div>
+      <h5>Post description here</h5>
+      <p>tag1</p> <p>tag2</p>  <p>tag3</p>
+    </>
+
+  );
+}
+}
